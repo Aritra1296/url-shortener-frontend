5ac0dd9460fff39eb80fe054dac1d37da9774570
Aritra1296 <aritrivu@gmail.com>
2024-02-26 14:40:06 +0530

    url change

--- src/app/Component.tsx
+++ src/app/Component.tsx
@@ -7,7 +7,7 @@ function Component({ shortUrl, fullUrl, buttonCallBack }: ComponentInterface) {
     <div className="flex justify-between w-full gap-3">
       <div className="text-wrap: wrap w-1/2">{fullUrl}</div>
       <div className="w-1/4 flex items-center justify-end">
-        http://localhost:5000/shortUrl/{shortUrl}
+        {process.env.NEXT_PUBLIC_REACT_APP_API_BASEURL}/{shortUrl}
       </div>
       <button
         className="w-1/4 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_REACT_APP_API_BASEURL,
+  baseURL:
+    process.env.NEXT_PUBLIC_REACT_APP_API_BASEURL || "http://localhost:5000",
   paramsSerializer: (params) => qs.stringify(params),
 });
 
